e50d3e9beeeac20483aad47bdcffbdb1d71232b5
Vedant1612 <113239505+Vedant1612@users.noreply.github.com>
2024-02-22 18:42:30 +0530

    first commit

---
+++ editor.js
@@ -0,0 +1,19 @@
+const blogTitleField = document.querySelector('.title');
+const articleFeild = document.querySelector('.article');
+
+// banner
+const bannerImage = document.querySelector('#banner-upload');
+const banner = document.querySelector(".banner");
+let bannerPath;
+
+const publishBtn = document.querySelector('.publish-btn');
+const uploadInput = document.querySelector('#image-upload');
+
+bannerImage.addEventListener('change', () => {
+    
+})
+
+uploadInput.addEventListener('change', () => {
+    
+})
+
